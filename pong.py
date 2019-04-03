0b5fc0fa4dcda5245b8393ff6e76f02854434074
SilasHenderson <46079402+SilasHenderson@users.noreply.github.com>
2019-04-03 08:58:58 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,7 +1,7 @@
 
 
 # --------------------------- Setup ------------------------------ #
-import numpy 										# Libraries
+import numpy 								# Libraries
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
@@ -16,41 +16,41 @@ boxVel  = 20
 w = 40
 h = 10
 
-pygame.init() 										# Start Pygame
+pygame.init() 								# Start Pygame
 pygame.display.set_mode((500, 500), 
 	             DOUBLEBUF|OPENGL)
 gluOrtho2D(0, 500, 0, 500)
 
-# -------------------------------  Functions -------------------------
-def drawBall(pos, rad): 							# Draw Ball Fcn
-	glBegin(GL_QUADS) 								# -- shape: quad
-	glColor3f(.8, .4, .4) 							# -- color: blue
+# -------------------------------  Functions ------------------------- #
+def drawBall(pos, rad): 						# Draw Ball Fcn
+	glBegin(GL_QUADS) 						# -- shape: quad
+	glColor3f(.8, .4, .4) 						# -- color: blue
 	for i in numpy.linspace(0, 361): 				# -- circle vertices
 		glVertex2f(pos[0] + rad*numpy.cos(i),  
 			       pos[1] + rad*numpy.sin(i))
 	glEnd()
 
-def drawBox(pos, w, h): 							# Draw Box Fcn
-	glBegin(GL_QUADS)								# -- shape: quad
-	glColor3f(.4, .4, .8)							# -- color: blue
-	glVertex(pos[0] - w, pos[1] - h)    			# -- box verticies
+def drawBox(pos, w, h): 						# Draw Box Fcn
+	glBegin(GL_QUADS)						# -- shape: quad
+	glColor3f(.4, .4, .8)						# -- color: blue
+	glVertex(pos[0] - w, pos[1] - h)    				# -- box verticies
 	glVertex(pos[0] + w, pos[1] - h)
 	glVertex(pos[0] + w, pos[1] + h)
 	glVertex(pos[0] - w, pos[1] + h)
 	glEnd()
 
-def keyboard(): 									# Keyboard
+def keyboard(): 							# Keyboard
 	key = pygame.key.get_pressed();
 	if key[pygame.K_LEFT]: 	boxPos[0] = boxPos[0] - boxVel
 	if key[pygame.K_RIGHT]: boxPos[0] = boxPos[0] + boxVel
 	if key[pygame.K_UP]:    resetFlag = 1
 
 def text2d(position, textString, size):     
-    font = pygame.font.Font (None, size)
-    textSurface = font.render(textString, True, (255,255,255,255), (0,0,0,255))     
-    textData = pygame.image.tostring(textSurface, "RGBA", True)     
-    glRasterPos3d(*position)     
-    glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
+   	font = pygame.font.Font (None, size)
+    	textSurface = font.render(textString, True, (255,255,255,255), (0,0,0,255))     
+    	textData = pygame.image.tostring(textSurface, "RGBA", True)     
+    	glRasterPos3d(*position)     
+    	glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
 
 def refresh(dt):
 	pygame.display.flip()
@@ -63,7 +63,6 @@ def refresh(dt):
 
 # -------------------- Loop ------------------
 while 1:
-
 	keyboard()
 	ballPos = ballPos + ballVel	
 	if ballPos[0] <   0:	ballVel[0] = -ballVel[0]

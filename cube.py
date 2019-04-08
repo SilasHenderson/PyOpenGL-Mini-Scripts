b5b9b8480660ce2814bbc62bd7a58dd7c43aae24
SilasHenderson <46079402+SilasHenderson@users.noreply.github.com>
2019-04-08 18:43:42 -0400

    Update cube.py

--- cube.py
+++ cube.py
@@ -1,23 +1,23 @@
 # ut Cube {Silas Henderson 2019}
 
-import pygame 												  # pygame
+import pygame 							  	# pygame
 from   pygame.locals import *
-from   OpenGL.GL     import *                              	  # opengl
+from   OpenGL.GL     import *                              	  	# opengl
 from   OpenGL.GLU    import *
 from   OpenGL.GLUT   import *  
 # ----------------------- Setup ---------------------------- 
-pygame.init(); 											  	  # Init
+pygame.init(); 							  	# Init
 glutInit();
 
-pygame.display.set_mode((500, 500),  DOUBLEBUF|OPENGL) 		  # Window
-gluPerspective(70, 1, .01, 1000) 							  # -- camEye
-glTranslate(0, 0, - 50)                                       # -- camPos
+pygame.display.set_mode((500, 500),  DOUBLEBUF|OPENGL) 		  	# Window
+gluPerspective(70, 1, .01, 1000) 				  	# -- camEye
+glTranslate(0, 0, - 50)                                           	# -- camPos
 
 # ----------------------- Loop -----------------------------
 while 1:														
-	glRotate(1, 1, 1, 1); 		glutSolidCube(20, 20, 20);    # rotate, render
-	pygame.display.flip();  	    pygame.time.delay(10);    # refresh, pause
-	glClear(GL_COLOR_BUFFER_BIT);                             # clear
+	glRotate(1, 1, 1, 1); 		glutSolidCube(20, 20, 20);    	# rotate, render
+	pygame.display.flip();  	    pygame.time.delay(10);    	# refresh, pause
+	glClear(GL_COLOR_BUFFER_BIT);                             	# clear
 
-	for event in pygame.event.get(): 						  # check input
-		if event.type == pygame.QUIT: pygame.quit(); quit();  # -- close (x)
+	for event in pygame.event.get(): 				# check input
+		if event.type == pygame.QUIT: pygame.quit(); quit();  	# -- close (x)

7a47bf183c3c19c5a85066deb541de515ef1e2a7
SilasHenderson <46079402+SilasHenderson@users.noreply.github.com>
2019-04-09 18:28:18 -0400

    Update bouncy.py

--- bouncy.py
+++ bouncy.py
@@ -9,18 +9,16 @@ import numpy
 
 pygame.init()
 pygame.display.set_mode((500, 500), DOUBLEBUF|OPENGL)
-gluOrtho2D(0, 500, 0, 500)      
-glClearColor(.05, .05, .05, 1)
+gluOrtho2D(    0, 500,   0, 500)      
+glClearColor(.05, .05, .05,   1)
 
-def refresh(dt): 									# Refresh Fcn
-	pygame.display.flip() 							# -- update
-	pygame.time.wait(dt) 							# -- pause
-	for event in pygame.event.get(): 				# -- check exit
+def refresh(dt): 							
+	pygame.display.flip() 						
+	pygame.time.wait(dt) 						
+	for event in pygame.event.get(): 			
 		if event.type == pygame.QUIT:
-			pygame.quit()
-			quit()
-	glClear(GL_COLOR_BUFFER_BIT| 					# -- clear
-		    GL_DEPTH_BUFFER_BIT)
+			pygame.quit();  quit();
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
 class Ball:
 	def __init__(self):
@@ -34,7 +32,7 @@ class Ball:
 		if self.pos[0] > 500: 	self.vel[0] = -self.vel[0]
 		if self.pos[1] <   0: 	self.vel[1] = -self.vel[1]
 		if self.pos[1] > 500: 	self.vel[1] = -self.vel[1]
-		
+	
 		self.pos = self.pos + self.vel
 
 	def draw(self): 								
@@ -45,7 +43,7 @@ class Ball:
 		               self.pos[1] + self.rad*numpy.sin(i))
 		glEnd()
 
-# ------------------------- Main -------------------------
+# ------------------------- Main -------------------------------------
 balls = [Ball() for i in range(0, 20)]
 
 while 1:
